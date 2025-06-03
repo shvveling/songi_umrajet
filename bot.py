74e97fa1df5bfa09336064e1b226509996fe7518
shvveling <95494203+shvveling@users.noreply.github.com>
2025-06-04 03:49:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,26 @@
-import logging
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.fsm.storage.memory import MemoryStorage
-
-from handlers import user
-
-import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.types import Message
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
 
-API_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
 
-logging.basicConfig(level=logging.INFO)
+@dp.message()
+async def start_handler(message: Message):
+    await message.answer(
+        "<b>Assalomu alaykum!</b>\n\n"
+        "Bu <b>UmraJet</b> xizmatlari botidir. Xizmatlarimizdan foydalanish uchun menyudan tanlang.\n\n"
+        "📲 @vip_arabiy\n📲 @V001VB\n🔗 Bot: @umrajet_bot\n📣 Kanal: @umrajet, @the_ravza"
+    )
 
-bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)
-
-# Routers
-dp.include_router(user.user_router)
-
-# Start command handler
-@dp.message(Command("start"))
-async def send_welcome(message):
-    await message.answer("Xush kelibsiz! /start buyrug‘ini qayta bosing.")
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(dp.start_polling(bot))
+    asyncio.run(main())
